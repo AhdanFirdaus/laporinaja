415555b64165aa278873012eeae4f47cf9ad6809
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-06-02 18:54:25 +0700

    ⚙️ config(routing): install and configure react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>main page</div>,
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
