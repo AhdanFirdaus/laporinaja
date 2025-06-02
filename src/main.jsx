d1310083ea4eb693638dc32e1a44b63d13adc418
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-06-02 19:30:41 +0700

    📌 chore(404): handle errorpage 404

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import './index.css'
+import ErrorPage from './Pages/404';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>main page</div>,
+    errorElement: <ErrorPage />,
   }
 ])
 
