9db2774fee2630d043ccbf7d7f5b7886fef6f977
RANGKI <marcelrangki@gmail.com>
2025-06-10 09:38:37 +0700

    ✨ feat(api): add checkk image size

---
+++ src/helper/checkImageSize.js
@@ -0,0 +1,14 @@
+const MAX_FILE_SIZE_MB = 1;
+
+export default function checkImageSize(file) {
+  const maxSize = MAX_FILE_SIZE_MB * 1024 * 1024; // in bytes
+
+  if (file.size > maxSize) {
+    return {
+      valid: false,
+      message: `Ukuran gambar terlalu besar. Maksimum ${MAX_FILE_SIZE_MB}MB.`,
+    };
+  }
+
+  return { valid: true };
+}
