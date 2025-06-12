603b689a8ee5c37e3c2d6baabf2a7f6072237def
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-06-12 13:19:13 +0700

    📌 chore(temp): temp

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
   console.log("✅ API route hit: /api/hello");
-  res.status(200).json({ message: "Hello from the backend!" });
+  res.status(200).json({ message: "Hello from the backend!!" });
 }
